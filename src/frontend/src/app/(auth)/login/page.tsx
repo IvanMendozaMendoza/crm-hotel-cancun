aed2b7e98e1ee6ced5c318bfb27621367c98fdfa
IvanMendozaMendoza <i60034162@gmail.com>
2025-07-18 10:24:32 -0600

    redirect logic

--- src/frontend/src/app/(auth)/login/page.tsx
+++ src/frontend/src/app/(auth)/login/page.tsx
@@ -1,6 +1,13 @@
 import { LoginForm } from "@/components/login-form"
+import { authOptions } from "@/lib/auth"
+import { getServerSession } from "next-auth"
+import { redirect } from "next/navigation"
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await getServerSession(authOptions)
+
+  if (session) redirect("/")
+
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
--- src/frontend/src/app/(root)/page.tsx
+++ src/frontend/src/app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
   const session = await getServerSession(authOptions);
 
-  console.log(session)
+  console.log(session);
+  
+  if (!session) redirect("/login");
 
-  return <div>{session ? `Hello ${session.user?.username}` : "Not signed in"}</div>;
+  return (
+    <div>{session ? `Hello ${session.user?.username}` : "Not signed in"}</div>
+  );
 }
