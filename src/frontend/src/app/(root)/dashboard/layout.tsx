f134c029c2dc561d1ae56e913ee7f16fc3099b88
IvanMendozaMendoza <i60034162@gmail.com>
2025-07-18 12:04:05 -0600

    dashboard cleaned up

---
+++ src/frontend/src/app/(root)/dashboard/layout.tsx
@@ -0,0 +1,32 @@
+import { AppSidebar } from "@/components/app-sidebar";
+
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+const DashboardLayout = async ({ children }: { children: React.ReactNode }) => {
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/login");
+
+  return (
+    <SidebarProvider
+      style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayout;
--- src/frontend/src/components/nav-main.tsx
+++ src/frontend/src/components/nav-main.tsx
@@ -10,6 +10,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { redirect } from "next/navigation"
 
 export function NavMain({
   items,
@@ -27,10 +28,11 @@ export function NavMain({
           <SidebarMenuItem className="flex items-center gap-2">
             <SidebarMenuButton
               tooltip="Quick Create"
+              onClick={()=>{redirect("/dashboard/upload")}}
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
             >
               <IconCirclePlusFilled />
-              <span>Quick Create</span>
+              <span>Upload file</span>
             </SidebarMenuButton>
             <Button
               size="icon"
