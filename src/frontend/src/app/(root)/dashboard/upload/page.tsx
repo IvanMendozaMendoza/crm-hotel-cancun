549749918f05c6ce0d1d72328956776ea302274b
IvanMendozaMendoza <i60034162@gmail.com>
2025-07-18 13:45:33 -0600

    file uploader added

--- src/frontend/src/app/(root)/dashboard/upload/page.tsx
+++ src/frontend/src/app/(root)/dashboard/upload/page.tsx
@@ -1,5 +1,11 @@
+import FileUploader from "@/components/file-uploader";
+
 const UploadPage = () => {
-  return <h1>Upload page</h1>;
+  return (
+    <section className="mx-auto md:mx-12 lg:mx-39 mt-12">
+      <FileUploader />
+    </section>
+  );
 };
 
 export default UploadPage;
