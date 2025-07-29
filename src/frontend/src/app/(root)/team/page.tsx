ed24868021aec1810f239ab5a24b3be3e6384925
IvanMendozaMendoza <i60034162@gmail.com>
2025-07-29 16:30:52 -0600

    search autocomplete off

--- src/frontend/src/app/(root)/team/page.tsx
+++ src/frontend/src/app/(root)/team/page.tsx
@@ -134,6 +134,8 @@ const TeamPage = () => {
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
               <Input
                 placeholder="Search"
+                type="search"
+                autoComplete="off"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 className="pl-10 bg-gray-900 border-gray-700 text-white placeholder-gray-400 w-full sm:w-64"
