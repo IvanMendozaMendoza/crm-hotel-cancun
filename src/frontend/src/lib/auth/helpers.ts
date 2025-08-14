14a4dc397812b22b854b277ff268e464deffc8da
IvanMendozaMendoza <i60034162@gmail.com>
2025-08-14 16:19:41 -0600

    Removes color picker component
    Removes the color picker component and related files.
    
    This component is no longer needed in the frontend application.

--- src/frontend/src/lib/auth/helpers.ts
+++ src/frontend/src/lib/auth/helpers.ts
@@ -2,8 +2,8 @@ import { getServerSession } from "next-auth";
 import { authOptions } from ".";
 
 export const getJwt = async (): Promise<string> => {
-    const session = await getServerSession(authOptions);
-    const jwt = session?.jwt;
-    if (!jwt) throw new Error("Not authenticated");
-    return jwt;
-}
+  const session = await getServerSession(authOptions);
+  const jwt = session?.jwt;
+  if (!jwt) throw new Error("Not authenticated");
+  return jwt;
+};
