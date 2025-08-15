2d938a933c41486de7ec63b677c5c02905064c76
IvanMendozaMendoza <i60034162@gmail.com>
2025-08-15 12:10:21 -0600

    Updates dashboard page text
    Updates the text on the dashboard page to indicate that it is still pending development.

--- src/frontend/src/app/(root)/dashboard/page.tsx
+++ src/frontend/src/app/(root)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 const Dashboard = () => {
   return (
     <div className="flex items-center justify-center min-h-[40vh] text-lg text-gray-400">
-      Dashboard page under development.
+      Dashboard page pending.
     </div>
   );
 };
